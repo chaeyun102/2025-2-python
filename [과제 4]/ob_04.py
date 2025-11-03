e76ae81ccd85e8aa5085ae17adef5ed060a312af
chaeyun102 <cheyunjung1002@gmail.com>
2025-11-03 15:06:45 +0900

    Add files via upload

---
+++ [과제 4]/ob_04.py
@@ -0,0 +1,20 @@
+class Course:
+    def __init__(self, name, scores=[]):
+        self.name = name 
+        self.scores = scores
+        self.avge =0
+    def add_score(self, s):
+        self.scores.append(s)
+
+    def avg(self):
+        self.avge = sum(self.scores) /len(self.scores)
+        return self.avge
+    
+    def info(self):
+        result = '과목:'+self.name+', 평균: '+str(self.avge)
+        return result
+
+c = Course('파이썬')
+c.add_score(80)
+c.add_score(90)
+print(c.info())
