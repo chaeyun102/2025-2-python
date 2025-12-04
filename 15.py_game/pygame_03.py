0f213bd2ca54e06ce84deb4dba2796f126c28ad4
chaeyun102 <cheyunjung1002@gmail.com>
2025-12-04 11:54:39 +0900

    Add files via upload

---
+++ 15.py_game/pygame_03.py
@@ -0,0 +1,15 @@
+import pygame 
+
+WIDTH, HEIGHT = 600, 400
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Step 3')
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((200,200,200))
+    pygame.display.flip()
+
+pygame.quit()
